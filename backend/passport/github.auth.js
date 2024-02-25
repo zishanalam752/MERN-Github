5f9101cd68b95d7f0ab5c82e1fd98304c46eb6a4
Md Zishan Alam <121496514+zishanalam752@users.noreply.github.com>
2024-02-25 18:15:32 +0530

    Update github.auth.js

--- backend/passport/github.auth.js
+++ backend/passport/github.auth.js
@@ -18,29 +18,33 @@ passport.deserializeUser(function (obj, done) {
 //   credentials (in this case, an accessToken, refreshToken, and GitHub
 //   profile), and invoke a callback with a user object.
 passport.use(
-	new GitHubStrategy(
-		{
-			clientID: process.env.GITHUB_CLIENT_ID,
-			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: "https://mern-github-07fw.onrender.com/api/auth/github/callback",
-		},
-		async function (accessToken, refreshToken, profile, done) {
-			const user = await User.findOne({ username: profile.username });
-			// signup
-			if (!user) {
-				const newUser = new User({
-					name: profile.displayName,
-					username: profile.username,
-					profileUrl: profile.profileUrl,
-					avatarUrl: profile.photos[0].value,
-					likedProfiles: [],
-					likedBy: [],
-				});
-				await newUser.save();
-				done(null, newUser);
-			} else {
-				done(null, user);
-			}
-		}
-	)
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: "https://mern-github-07fw.onrender.com/api/auth/github/callback",
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      try {
+        const user = await User.findOne({ username: profile.username });
+
+        if (!user) {
+          const newUser = new User({
+            name: profile.displayName,
+            username: profile.username,
+            profileUrl: profile.profileUrl,
+            avatarUrl: profile.photos[0].value,
+            likedProfiles: [],
+            likedBy: [],
+          });
+          await newUser.save();
+          done(null, newUser);
+        } else {
+          done(null, user);
+        }
+      } catch (error) {
+        done(error, null);
+      }
+    }
+  )
 );
